516f7f9b26f5459968ebcd9622da1fbd8378a642
OfflineHTTPServer <125279720+OfflineHTTPServer@users.noreply.github.com>
2023-12-14 19:07:13 +0100

    Last Preperations for Launch

--- src/middleware/main.ts
+++ src/middleware/main.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from "express";
 
 export default function Main(req: Request, res: Response, next: () => void) {
-    console.log(req.path);
+    console.log(`Request: ${req.path} - ${new Date()}`);
     next();
 }
